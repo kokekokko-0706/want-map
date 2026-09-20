623b98c299cb2d096487043cf7ea3afed407dd75
kokekokko-0706 <kokekokko.0706@gmail.com>
2026-09-20 21:30:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='want-map-cache-v1';
+const CACHE='want-map-cache-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
