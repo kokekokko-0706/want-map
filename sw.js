a15dfa3855495abdb931f34380105789a54ad544
kokekokko-0706 <kokekokko.0706@gmail.com>
2026-09-20 23:02:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
-const CACHE='want-map-v8';
+const CACHE='want-map-v10';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return res}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin) return;
+  const isCore=/\/index\.html$|\/manifest\.webmanifest$|\/icon-(180|512)\.png$|\/sw\.js$/.test(u.pathname);
+  if(isCore){
+    e.respondWith(fetch(e.request).then(res=>{
+      const c=res.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return res;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+  } else {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const c=res.clone(); caches.open(CACHE).then(x=>x.put(e.request,c)); return res;
+    }).catch(()=>caches.match('./index.html'))));
+  }
+});
